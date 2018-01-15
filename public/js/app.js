e2bc98aa375e2342ebb658c86e28b6278557c23a
Tech-A23 <tech-a23@Tech-A23s-MacBook-Air.local>
2018-01-15 23:11:05 +0000

    Edited CSS for add new student section, title and form centered and coloured

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,9 @@
 $(document).ready(function(){
 	console.log('working');
 });
+
+// Set input box to equal the placeholder length - not working yet
+// var input = document.querySelectorAll('input');
+// for(i=0; i<input.length; i++){
+//     input[i].setAttribute('size',input[i].getAttribute('placeholder').length);
+// }
