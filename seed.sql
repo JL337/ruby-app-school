1bf85b6adee96ba26028990f506970bbf9ffc16b
Tech-A23 <tech-a23@Tech-A23s-MacBook-Air.local>
2018-01-16 00:28:58 +0000

    Minor student DB name changes and editing placeholder values

--- seed.sql
+++ seed.sql
@@ -17,7 +17,7 @@ INSERT INTO students (name, gender, age, best_subject, year) VALUES ('Name 3' , 
 INSERT INTO students (name, gender, age, best_subject, year) VALUES ('Name 4' , 'Gender 4', '4', 'Best subject 4', '4');
 
 
-UPDATE students SET name='Stanislav Ferapoont', gender='Male', age='15', best_subject='Physics', year='11' WHERE id = '1';
-UPDATE students SET name='Mordred Lesley', gender='Male', age='15', best_subject='English', year='11' WHERE id = '2';
-UPDATE students SET name='Elisabeth Eileen', gender='Female', age='14', best_subject='Biology', year='10' WHERE id = '3';
-UPDATE students SET name='Harrieta Sofya', gender='Female', age='14', best_subject='Mathematics', year='10' WHERE id = '4';
+UPDATE students SET name='Ordis Ferapoont', gender='Male', age='15', best_subject='Physics', year='11' WHERE id = '1';
+UPDATE students SET name='Mordecai Cedillo', gender='Male', age='15', best_subject='English', year='11' WHERE id = '2';
+UPDATE students SET name='Monika Cateline', gender='Female', age='14', best_subject='Biology', year='10' WHERE id = '3';
+UPDATE students SET name='Sayori Milana', gender='Female', age='14', best_subject='Mathematics', year='10' WHERE id = '4';
